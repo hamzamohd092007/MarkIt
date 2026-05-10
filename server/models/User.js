ee3becc0142a992e0bb5bf009221ff7112343531
Mohd Hamza <hamzamohd092007@gmail.com>
2026-05-10 10:59:37 +0530

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
     fullName: {type: String, required: true},
-    avatar: { type: String, default: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrzK1R5eFyx2t3yRYdFNEMGkucJnl_txpmoQ&s" },
+    avatar: { type: String, default: "/defaultAvatar" },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true, select: false }
 }, { timestamps: true });
