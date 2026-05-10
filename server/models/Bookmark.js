b1d705e65610ed869125e9aad4d66d35491e5c4e
Mohd Hamza <hamzamohd092007@gmail.com>
2026-05-10 11:00:08 +0530

    Update Bookmark.js

--- server/models/Bookmark.js
+++ server/models/Bookmark.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const bookmarkSchema = new mongoose.Schema({
-    favicon: { type: String, default: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRK_8I4bFhQo_yon74-SpuRM4a-EeuAp5VB5A&s" },
+    favicon: { type: String, default: "/defaultFavicon" },
     title: { type: String, required: true },
     url: { type: String, required: true },
     description: { type: String, required: true },
